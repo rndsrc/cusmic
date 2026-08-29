9c1d304bcda4b8efdd1bf48ac3fed13864d54b00
Chi-kwan Chan <rndsrc@gmail.com>
2026-08-29 00:08:43 -0700

    Put pixel data into dataclass `Image`

---
+++ mod/cusmic/image.py
@@ -0,0 +1,30 @@
+# Copyright 2026 Chi-kwan Chan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from dataclasses import dataclass
+
+from cupy import ndarray as Array
+
+
+@dataclass
+class Image:
+    """Pixels and calibration data"""
+
+    data:           Array
+    error:          Array | None = None
+    mask:           Array | None = None
+    background:     Array | None = None
+    effective_gain: Array | None = None
+    readnoise:      float | None = None
